e9a715f7c694d8bc53aeb6f5c169ffbcc0db30c6
lily <lceciledesign@gmail.com>
2022-11-02 17:57:25 +0100

    Search typeahead * call to get country data and search term observable combined with switchMap

--- src/app/country-search/country-search.component.ts
+++ src/app/country-search/country-search.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { debounceTime, delay, distinctUntilChanged, map, Observable, of } from 'rxjs';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { debounceTime, delay, distinctUntilChanged, filter, fromEvent, map, Observable, of, pluck, switchMap, tap } from 'rxjs';
 import { countryInfo } from '../models/country-info';
 import { CountriesService } from '../services/countries.service';
 
@@ -8,46 +8,37 @@ import { CountriesService } from '../services/countries.service';
   templateUrl: './country-search.component.html',
   styleUrls: ['./country-search.component.less']
 })
-export class CountrySearchComponent implements OnInit {
+export class CountrySearchComponent {
   countryInfo: countryInfo;
   countryList: Array<any> = [];
   searchKeyupObservable: Observable<any>;
+  input: any = '';
+  term: any;
+  selectedCountry: string;
   constructor(private countriesService: CountriesService) {
   }
 
-  ngOnInit() {
-    this.countriesService.getAllCountries().pipe(
-      map(res => {
-        res.map((country: any) => {
-          const countryName = country.name.common;
-          this.countryList.push(countryName);
-        })
+  countrySelected(country: any) {
+    this.selectedCountry = this.countryList[country];
+    this.countriesService.getCountryData(this.selectedCountry).subscribe(data => {
+      this.countryInfo = new countryInfo(data[0]);
+    })
+  }
+
+  searchCountries() {
+    this.input = document.getElementById('search-input');
+    const search$ = fromEvent(this.input, 'keyup')
+      .pipe(
+        map((search: any) => {
+          this.term = search.target.value;
+        }),
+        switchMap(() => this.countriesService.getCountryData(this.term)));
+    search$.subscribe(countries => {
+      this.countryList = []
+      countries.map((country: any) => {
+        this.countryList.push(country.name.common);
       })
-    ).subscribe();
+    })
   }
 
-
-  filterCountries(value: any) {
-    // return of(this.countryList.filter(country => country.indexOf(value) > 1)).pipe(
-    //   delay(100)
-    // );
-
-    return of(this.countryList.filter((country) => {
-      console.log(country.indexOf(value), 'country')
-    }))
-  }
-
-
-
-  searchCountries(value: any) {
-    this.filterCountries(value.key);
-    this.searchKeyupObservable = value.key;
-    console.log(value.key, 'value')
-    this.searchKeyupObservable.pipe(
-      debounceTime(300),
-      map(key => key.target.value),
-      distinctUntilChanged(),
-    )
-  }
 }
-
--- src/app/models/country-info.ts
+++ src/app/models/country-info.ts
@@ -8,7 +8,7 @@ export class countryInfo {
   constructor(value: any) {
     this.name = value.name.common;
     this.capital = value.capital;
-    this.continent = value.continents;
+    this.continent = value.continents[0];
     this.flag = value.flag;
     this.language = value.languages[Object.keys(value.languages)[0]]
     this.independent = value.independent;
